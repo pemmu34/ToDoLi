ad35aa559e9668dd1258d40d09eed73dc3e53464
pemmu34 <artem.bubucadhkin.123@gmail.com>
2025-05-03 12:18:32 +0300

    initial commitlast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,6 @@ import {FormsModule} from '@angular/forms';
 export class AppComponent {
   public tasksManager: TasksManager = inject(TasksManager);
 
-  hoveredTaskId: string | null = null;
   inputValue = signal('');
   uncompletedTasksCount = this.tasksManager.countFalseTasks();
 
